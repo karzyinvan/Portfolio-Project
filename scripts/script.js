01a6a15d6e09cc69d93a66c651d3ce26417fd2fb
Kara <karamcclure81@gmail.com>
2017-02-13 13:16:34 -0800

    Mobile Nav menu
    amended mobile menu …again….

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+    //Javascript document
+
+    var $mainnav = $('.main-nav');
+
+    var mainNavTopPos = $mainnav.offset().top;
+
+    $(window).scroll(function(){
+    	if($(window).scrollTop() > mainNavTopPos){
+    		$mainnav.addClass('sticky');
+    	}else{
+    		$mainnav.removeClass('sticky');
+    	}
+
+    });
+
+    AOS.init({
+      offset: 200,
+      duration: 1200,
+      // easing: 'ease-in-sine',
+      // delay: 100,
+    });
+
+    //mobile button//
+    // button script
+
+
+    var $body = $('body');
+    var $btnMobileMenu = $('.btn-mobile-menu');
+
+    // click event handler for the mobile menu button
+    $btnMobileMenu.click(function(){
+
+
+    	// toggle the show-mobile-menu class
+    	// on the body element
+     $body.toggleClass('show-mobile-menu');
+
+    }); // end click event handler for the mobile menu button

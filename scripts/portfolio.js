c6478000a979d6f3671cac284ede56d6e24ca3bd
Kara <karamcclure81@gmail.com>
2017-02-12 21:52:01 -0800

    button-mobile-menu
    added the css for the iPhone6 media query

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -20,5 +20,22 @@
       // delay: 100,
     });
 
+    
+// button script
+
+
+var $body = $('body');
+var $btnMobileMenu = $('.btn-mobile-menu');
+
+// click event handler for the mobile menu button
+$btnMobileMenu.click(function(){
+  
+
+  // toggle the show-mobile-menu class
+  // on the body element
+ $body.toggleClass('show-mobile-menu');
+  
+}); // end click event handler for the mobile menu button
+
 
 

544a84fc53d0d590ceac1281be235c3f99af12b0
Prototype444 <prototype@ProtoBook.local>
2017-02-18 13:31:47 -0800

    Fixed Menu Overlay
    Change the default visibility. change the behaviour on clicks so that
    it opens and closes properly.

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
-    $('.hamburger').click(function(){
-        $(this).toggleClass('open');
-        $('.menu-overlay').toggleClass('open');
-     });
+  $('.hamburger').click(function(){
+    $(this).toggleClass('open');
+      $('.menu-overlay').toggleClass('open');
+    });
+
+  $('.menu-overlay a').click(function(){
+      $('.hamburger').toggleClass('open');
+      $('.menu-overlay').toggleClass('open');
+  });
 });
